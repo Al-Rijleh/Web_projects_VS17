66a8bd0788754993733edd8fc68080e8ae461760
M_Al-Rijleh <M_Al-Rijleh@200DTPG1010>
2019-06-17 13:17:47 -0400

    w1

--- TestOpnGit/WebApplication1/WebForm1.aspx.cs
+++ TestOpnGit/WebApplication1/WebForm1.aspx.cs
@@ -16,7 +16,7 @@ namespace WebApplication1
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            TextBox1.Text = "Abcd";
+            TextBox1.Text = "Abc";
         }
     }
 }
